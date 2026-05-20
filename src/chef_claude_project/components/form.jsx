7d68a113e2a853df4a3e30e951e0acb2fa55cf50
Nyakwara Festus <festusnyakwara@Gmail.com>
2026-05-20 11:59:06 +0300

    Added a recipe container and modified necessary areas

--- src/chef_claude_project/components/form.jsx
+++ src/chef_claude_project/components/form.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { useAtom } from 'jotai'
+import { ingredientsAtom } from '../states/atoms'
 
 const Form = () => {
-
-    const ingredients = [
-        'tomato','onion','garlic','olive oil',
-    ]
+    const [ingredients, setIngredients] = useAtom(ingredientsAtom)
 
     const ingredientsList = ingredients.map((ingredient, index) => (
         <li key={index} className='text-sm text-gray-700'>{ingredient}</li>
@@ -15,13 +14,14 @@ const Form = () => {
         const formData = new FormData(e.currentTarget)
         const newIngredient = formData.get('Ingredients')
         if (newIngredient) {
-            ingredients.push(newIngredient)
+            setIngredients(prev => [...prev, newIngredient])
         } 
+        e.currentTarget.reset()
     }
 
   return (
-    <div>
-        <form className='flex gap-3 items-center-safe justify-items-center-safe p-3 w-[100%] border-2 border-gray-300 rounded-sm bg-white' 
+    <div className='max-w-md mx-auto mt-10 p-6' >
+        <form className='flex gap-3 items-center-safe justify-items-center-safe p-3 w-[100%] order-2 border-gray-300 rounded-sm bg-white' 
             onSubmit={handleSubmit}>
             <div className="focus-within:ring-2 focus-within:ring-orange-300 rounded-sm">
                 <input aria-label="ingredients" name='Ingredients' type="text" placeholder='e.g salagna'
@@ -36,6 +36,15 @@ const Form = () => {
             <ul className='list-disc list-inside'>
                 {ingredientsList}
             </ul>
+            <div className="flex items-center gap-4 mt-6 p-4 border-2 border-gray-300 rounded-sm bg-white">
+                <div className="flex flex-col gap-1">
+                    <h1 className='text-2xl font-bold'>Ready for Recipe?</h1>
+                    <p>Generate your recipe based on your ingredients!</p>
+                </div>
+                <button className='bg-orange-500 text-white px-4 py-2 rounded-sm hover:bg-orange-600 transition-colors duration-200'>
+                    Generate Recipe
+                </button>
+            </div>
         </div>
     </div>
     )
